32a72ee86fff9b87981d7bf76d7d8d1da41dc317
mujeebgondal <mujeeb4025@gmail.com>
2025-09-17 23:28:24 +0500

    # Simulates a Magic 8-Ball that gives random answers to user questions.

--- majic 8.py
+++ majic 8.py
@@ -1,21 +1,25 @@
 import random
-q=input("Ask Question: ")
+q=input("Question: ")
 n=random.randint(1, 9)
 if n == 1:
-  print("Yes - definitely.")
+  ans='Yes - definitely.'
 elif n == 2:
-  print("It is decidedly so.")
+  ans='It is decidedly so.'
 elif n == 3:
-  print("Without a doubt")
+  ans='Without a doubt.'
 elif n == 4:
-  print("Reply hazy , try again")
+  ans='Reply hazy , try again.'
 elif n == 5:
-  print("Ask again later")
+  ans='Ask again later.'
 elif n == 6:
-  print("Better not tell yoy now")
+  ans='Better not tell yoy now.'
 elif n == 7:
-  print("My source say no")
+  ans='My source say no.'
 elif n == 8:
-  print("Outlook not so good")
+  ans='Outlook not so good.'
+elif n == 9:
+  ans='Very doubtful.'
 else :
-  print("Very doubtful")
+  ans='error'
+print("majic 8 ball", ans)
+
